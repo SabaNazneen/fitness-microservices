05ba2aa1341398caef9e280d2d17c541023c1bc9
Saba Nazneen <nazeema2078@gmail.com>
2026-08-27 11:32:57 +0530

    Initial commit

---
+++ activityservice/src/main/java/com/fitness/activityservice/service/ActivityService.java
@@ -0,0 +1,41 @@
+package com.fitness.activityservice.service;
+
+import com.fitness.activityservice.ActivityRepository;
+import com.fitness.activityservice.dto.ActivityRequest;
+import com.fitness.activityservice.dto.ActivityResponse;
+import com.fitness.activityservice.model.Activity;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ActivityService {
+    private final ActivityRepository activityRepository;
+
+    public ActivityResponse trackActivity(ActivityRequest request) {
+        Activity activity = Activity.builder()
+                .userId(request.getUserId())
+                .type(request.getType())
+                .duration(request.getDuration())
+                .caloriesBurned(request.getCaloriesBurned())
+                .startTime(request.getStartTime())
+                .additionalMetrics(request.getAdditionalMetrics())
+                .build();
+        Activity savedActivity = activityRepository.save(activity);
+        return mapToResponse(savedActivity);
+    }
+
+    private ActivityResponse mapToResponse(Activity savedActivity) {
+        ActivityResponse activityResponse = new ActivityResponse();
+        activityResponse.setId(savedActivity.getId());
+        activityResponse.setUserId(savedActivity.getUserId());
+        activityResponse.setType(savedActivity.getType());
+        activityResponse.setDuration(savedActivity.getDuration());
+        activityResponse.setStartTime(savedActivity.getStartTime());
+        activityResponse.setCaloriesBurned(savedActivity.getCaloriesBurned());
+        activityResponse.setUpdatedAt(savedActivity.getUpdatedAt());
+        activityResponse.setCreatedAt(savedActivity.getCreatedAt());
+        activityResponse.setAdditionalMetrics(savedActivity.getAdditionalMetrics());
+        return activityResponse;
+    }
+}
---
+++ userservice/src/main/java/com/fitness/userservice/services/UserService.java
@@ -0,0 +1,54 @@
+package com.fitness.userservice.services;
+
+import com.fitness.userservice.dto.RegisterRequest;
+import com.fitness.userservice.dto.UserResponse;
+import com.fitness.userservice.models.User;
+import jdk.jshell.spi.ExecutionControl;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+    public UserResponse register(RegisterRequest request) {
+
+        if(userRepository.existsByEmail(request.getEmail())){
+            throw new RuntimeException("Email already exists");
+        }
+        User user = new User();
+        user.setEmail(request.getEmail());
+        user.setPassword(request.getPassword());
+        user.setFirstname(request.getFirstname());
+        user.setLastname(request.getLastname());
+
+        User savedUser = userRepository.save(user);
+        UserResponse userResponse = new UserResponse();
+        userResponse.setId(savedUser.getId());
+        userResponse.setEmail(savedUser.getEmail());
+        userResponse.setFirstname(savedUser.getFirstname());
+        userResponse.setLastname(savedUser.getLastname());
+        userResponse.setPassword(savedUser.getPassword());
+        userResponse.setCreatedAt(savedUser.getCreatedAt());
+        userResponse.setUpdatedAt(savedUser.getUpdatedAt());
+        return userResponse;
+    }
+
+    public UserResponse getUserProfile(String userId) {
+
+        User savedUser = userRepository.findById((userId))
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        UserResponse userResponse = new UserResponse();
+
+        userResponse.setId(savedUser.getId());
+        userResponse.setEmail(savedUser.getEmail());
+        userResponse.setFirstname(savedUser.getFirstname());
+        userResponse.setLastname(savedUser.getLastname());
+        userResponse.setPassword(savedUser.getPassword());
+        userResponse.setCreatedAt(savedUser.getCreatedAt());
+        userResponse.setUpdatedAt(savedUser.getUpdatedAt());
+
+        return userResponse;
+    }
+}
